0b53cf96e7872752dc18de2fcdccec38ead3aaee
Imi i nazwisko <example@email.com>
2022-10-25 21:25:49 +0200

    lab2_amendments

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -2,20 +2,42 @@ import unittest
 
 from ..Konto import Konto
 
+
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
         pierwsze_konto = Konto("Dariusz", "Januszewski", "02252807357")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.imie, "Dariusz",
+                         "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski",
+                         "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-        self.assertEqual(pierwsze_konto.pesel, "02252807357", "Pesel nie został zapisany!")
-        self.assertEqual(len(pierwsze_konto.pesel), 11, "Niepoprawny pesel!")
+        self.assertEqual(pierwsze_konto.pesel, "02252807357",
+                         "Pesel nie został zapisany!")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Poprawny pesel!")
 
-    def test_tworzenie_konta2(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski", "02252807357", "PROM_374")
-        self.assertEqual(pierwsze_konto.saldo, 50, "Kod promocyjny aktywowany!")
-        
-    def test_tworzenie_konta2(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski", "59252807357", "PROM_374")
-        self.assertEqual(pierwsze_konto.saldo, 0, "Kod promocyjny nieaktywny dla staruchów!") 
+    def pesel_length_test(self):
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "022528073571")
+        self.assertEqual(pierwsze_konto.pesel,
+                         "Niepoprawny pesel!", "Pesel jest poprawny")
+        drugie_konto = Konto("Dariusz", "Januszewski", "022528073")
+        self.assertEqual(drugie_konto.pesel,
+                         "Niepoprawny pesel!", "Pesel jest poprawny")
+
+    def promo_code_for_under_60(self):
+        pierwsze_konto = Konto("Dariusz", "Januszewski",
+                               "02252807357", "PROM_374")
+        self.assertEqual(pierwsze_konto.saldo, 50,
+                         "Kod promocyjny aktywowany!")
+
+    def promo_code_for_over_60(self):
+        pierwsze_konto = Konto("Dariusz", "Januszewski",
+                               "59252807357", "PROM_374")
+        self.assertEqual(pierwsze_konto.saldo, 0,
+                         "Kod promocyjny nieaktywny dla staruchów!")
+
+    def wrong_promo_code(self):
+        pierwsze_konto = Konto("Dariusz", "Januszewski",
+                               "59252807357", "PROM_3742")
+        self.assertEqual(pierwsze_konto.saldo, 0,
+                         "nie prawidłowy kod")
