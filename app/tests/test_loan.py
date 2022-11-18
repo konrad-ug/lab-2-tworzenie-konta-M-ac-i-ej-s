e1c84939f724d62dbe5abde26afbb833eadfbcc1
Maciej  Słupianek <maciej.slupianekge@gmail.com>
2022-11-18 15:26:46 +0100

    bugfix_3

--- app/tests/test_loan.py
+++ app/tests/test_loan.py
@@ -7,7 +7,7 @@ class TestLoan(unittest.TestCase):
     def setUp(self):
         self.konto = Konto("Darek", "Maciborek", "02252807358")
         
-    @parameterized([
+    @parameterized.expand([
     ([200,20,100,400,40], 200, True, 200),
     ([200,-20,100,400,40], 200, False, 0),
     ([100,400,40], 200, False, 0),
