4e49dadcee460ad7acf206d59bcff3cddf9ad271
Maciej  Słupianek <maciej.slupianekge@gmail.com>
2022-11-18 15:17:18 +0100

    parameterized

--- app/tests/test_loan.py
+++ app/tests/test_loan.py
@@ -2,31 +2,18 @@ import unittest
 from ..Konto import Konto
 
 class TestLoan(unittest.TestCase):
-    def test_loan_positive(self):
-        konto = Konto("Darek", "Maciborek", "02252807358")
-        konto.saldo = 400
-        konto.historia = [200,20,100,400,40]
-        self.assertEqual(konto.takeLoan(200), True, "Kredyt nie został udzielony!")
-        self.assertEqual(konto.saldo, 600, "nie poprawna suma pieniedzy")
 
-    def test_loan_negative1(self):
-        konto = Konto("Darek", "Maciborek", "02252807358")
-        konto.saldo = 400
-        konto.historia = [200,-20,100,400,40]
-        self.assertEqual(konto.takeLoan(200), False, "Kredyt został udzielony!")    
-        self.assertEqual(konto.saldo, 400, "nie poprawna suma pieniedzy")
+    def setUp(self):
+        self.konto = Konto("Darek", "Maciborek", "02252807358")
+        
+    @parameterized([
+    ([200,20,100,400,40], 200, True, 200),
+    ([200,-20,100,400,40], 200, False, 0),
+    ([100,400,40], 200, False, 0),
+    ([10,10,10,10,10], 200, False, 0),
+    ])
+    def test_pow(self, historia, kwota, oczekiwany_wynik, oczekiwane_saldo):
+        self.konto.historia = historia
+        self.assertEqual(self.konto.takeLoan(kwota), oczekiwany_wynik, "Kredyt nie został udzielony!")
+        self.assertEqual(self.konto.saldo, oczekiwane_saldo, "nie poprawna suma pieniedzy")
 
-    def test_loan_negative2(self):
-        konto = Konto("Darek", "Maciborek", "02252807358")
-        konto.saldo = 400
-        konto.historia = [100,400,40]
-        self.assertEqual(konto.takeLoan(200), False, "Kredyt został udzielony!")
-        self.assertEqual(konto.saldo, 400, "Kredyt został udzielony!")       
-
-    def test_loan_negative3(self):
-        konto = Konto("Darek", "Maciborek", "02252807358")
-        konto.saldo = 140
-        konto.historia = [10,10,10,10,10]
-        self.assertEqual(konto.takeLoan(200), False, "Kredyt został udzielony!")    
-        self.assertEqual(konto.saldo, 140, "nie poprawna suma pieniedzy")        
- 
